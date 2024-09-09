ad0b1a71d2e7e6db7082bf05985975e389483620
yshy <lichi621@outlook.com>
2024-09-09 13:05:18 +0800

    第3章 初识Django路由系统相关代码更新

--- MiniDouban/urls.py
+++ MiniDouban/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from movie import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("moviehome", views.moviehome),
+    path('', views.home),
 ]
--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def moviehome(request):
+    return HttpResponse('<h1>欢迎来到movie应用首页</h1>')
+
+def home(request):
+    return HttpResponse('<h1>欢迎来到项目首页</h1>')

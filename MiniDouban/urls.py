10774b8e3605b08eb464052ebac2b85b529c1493
OSYouth <lichi621@outlook.com>
2024-09-16 23:52:01 +0800

    第5章模型第2次备课内容部分 这要每天打卡github还是有点难，今天临睡觉忽然想起还没有敲代码，马上敲了10分钟的，这以后还是要把敲代码这个事情尽量放在每天的第一件

--- MiniDouban/urls.py
+++ MiniDouban/urls.py
@@ -17,10 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from movie import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("moviehome", views.moviehome, name='moviehome'),
     path('', views.home, name='home'),
-    path('signup/', views.signup, name='signup')
+    path('signup/', views.signup, name='signup'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

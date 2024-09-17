ed2b6b036991a5e8ca866450af0db1b7420c806f
OSYouth <lichi621@outlook.com>
2024-09-17 23:17:52 +0800

    第5章模型第2次备课全部内容 没想到中秋节的晚上我居然在辛苦备课，还到了晚上十一点半，但愿以后学生或者读者看了能够给我点个赞吧

--- movie/models.py
+++ movie/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 class Movie(models.Model) :
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=250)
-    image = models.ImageField(upload_to='movie/images/' )
-    url = models.URLField(blank=True)
+    title = models.CharField(max_length=100, verbose_name="电影名")
+    description = models.CharField(max_length=250, verbose_name="电影简介")
+    image = models.ImageField(upload_to='movie/images/', verbose_name="电影海报" )
+    url = models.URLField(blank=True, verbose_name="电影资源链接")
+
+    class Meta:
+        verbose_name = "电影"
+        verbose_name_plural = "电影"

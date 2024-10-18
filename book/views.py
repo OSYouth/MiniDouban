ed5f175d4687aee63554e78886425391839e46cb
OSYouth <lichi621@outlook.com>
2024-10-18 17:00:03 +0800

    第8章 第2次备课5 主要是分页功能的实现

--- book/views.py
+++ book/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from .models import Book
+from django.core.paginator import Paginator
 
 def bookhome(request):
     searchTerm = request.GET.get('searchBook')
     if searchTerm:
-        books = Book.objects.filter(title__contains=searchTerm)
+        book_list = Book.objects.filter(title__contains=searchTerm)
     else:
-        books = Book.objects.all()
+        book_list = Book.objects.all()
+    paginator = Paginator(book_list, 15)
+    page_number = request.GET.get('page', 1)
+    books = paginator.page(page_number)
+
     return render(request, 'bookhome.html', {'searchTerm':searchTerm, 'books':books})
--- movie/views.py
+++ movie/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Movie
+from django.core.paginator import Paginator
 
 # Create your views here.
 def moviehome(request):
     searchTerm = request.GET.get('searchMovie')
     if searchTerm:
-        movies = Movie.objects.filter(title__contains=searchTerm)
+        movie_list = Movie.objects.filter(title__contains=searchTerm)
     else:
-        movies = Movie.objects.all()
+        movie_list = Movie.objects.all()
+    paginator = Paginator(movie_list, 15)
+    page_number = request.GET.get('page', 1)
+    movies = paginator.page(page_number)
     return render(request, 'moviehome.html', {'searchTerm':searchTerm, 'movies':movies})
 
 def home(request):

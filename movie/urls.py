7e24cb01a523cd27301f841594568b4d2436d2d3
OSYouth <lichi621@outlook.com>
2024-11-02 22:19:28 +0800

    第10章 第19次课 处理评论1 修改评论

--- movie/urls.py
+++ movie/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("moviehome", views.moviehome, name='moviehome'),
     path('<int:movie_id>', views.moviedetail, name='moviedetail'),
     path('<int:movie_id>/createmoviereview', views.createmoviereview, name='createmoviereview'),
+    path('review/<int:review_id>', views.updatemoviereview, name='updatemoviereview'),
 ]
--- movie/views.py
+++ movie/views.py
@@ -44,3 +44,17 @@ def createmoviereview(request, movie_id):
             return redirect('moviedetail',newReview.movie.id)
         except ValueError:
             return render(request,'createmoviereview.html', {'form':ReviewForm, 'error':'非法数据'})
+
+def updatemoviereview(request, review_id) :
+    review = get_object_or_404(Review, pk=review_id, user=request.user)
+    if request.method == 'GET':
+        form = ReviewForm(instance=review)
+        return render(request, 'updatemvoiereview.html', {'review':review, 'form':form})
+    else:
+        try:
+            form = ReviewForm(request.POST, instance=review)
+            form.save()
+            return redirect('moviedetail', review.movie.id)
+        except ValueError:
+            return render(request, 'updatemoviereview.html', {'review':review, 'form':form, 'error':'提交非法数据'})
+
